882f3d38979e4bc4ba1018d078c80a45af55ee3c
Po-Jen Lai <berln579@gmail.com>
2015-02-11 22:13:59 +0800

    finish l2Distance pseudo code, need to modify to matlab style and test

--- hw1/l2Distance.m
+++ hw1/l2Distance.m
@@ -8,3 +8,21 @@ function distance = l2Distance(X1, X2)
 distance = zeros(size(X1,1), size(X2,1));
 
 % Your code here
+
+for q_row=1:size(X1,1)
+	for d_row=1:size(X2,1)
+		inner_product = 0;
+		q_norm = 0;
+		d_norm = 0;
+
+		for dim=1:size(X1,2)
+			inner_product = inner_product + X1(q_row, dim)*X2(d_row, dim);
+			q_norm = q_norm + X1(q_row, dim)^2;
+			p_norm = p_norm + X2(d_row, dim)^2;
+		end
+		
+		q_norm = sqrt(q_norm);
+		d_norm = sqrt(d_norm);
+		distance(q_row, d_row) = inner_product / (q_norm*d_norm);
+	end
+end
