7707d5246766753383a74ac9e9d3affb73dc3ef2
Po-Jen Lai <berln579@gmail.com>
2015-02-15 23:45:27 +0800

    Update calculateMAP pseudo code
    1.need to finish sorting
    2.need further testing by checking if the MAP is 0.102

--- hw1/calculateMAP.m
+++ hw1/calculateMAP.m
@@ -11,3 +11,30 @@ function [meanAveragePrecision] = calculateMAP(dist, queryId, databaseId)
 meanAveragePrecision = 0;
 
 % Your code here
+
+% Retrieved number |A| = n (all should be considered as retrieved)
+% Relevant |R| should be calculated by myself, use databaseId
+
+% Sort the m*n distance matrix one row in a time
+
+% Generate queryResult (m*n matrix), but the content is databaseId
+
+% Calculate AP
+averagePrecision = 0;
+correctNum=0;
+
+for queryNum=1:m
+  averagePrecision=0;
+  correctNum=0;
+  
+  for resultNum=1:n
+      if(queryId(queryNum) == databaseId(queryResult(queryNum, resultNum)))
+        correctNum = correctNum+1;
+        averagePrecision = averagePrecision + correctNum/resultNum;
+      end
+  end
+  averagePrecision = averagePrecision/correctNum;
+  meanAveragePrecision = meanAveragePrecision + averagePrecision;
+end
+
+meanAveragePrecision = meanAveragePrecision/m;
